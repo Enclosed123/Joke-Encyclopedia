ac5e66004ebbc3b135cb67a6fc8de343b8fded3d
baohan <373751805@qq.com>
2020-01-05 13:47:45 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<view class="uni-list">
+		<view class="uni-list-cell">
+		<view class="uni-list-cell-navigate uni-navigate-right">
+		列表一
+		</view>
+		</view>
+		<view class="uni-list-cell" style="font-size: 16;">
+		<view class="uni-list-cell-navigate uni-navigate-right">
+		列表一
+		</view>
+		</view>
+		<view class="uni-list-cell">
+		<view class="uni-list-cell-navigate uni-navigate-right">
+		列表一
+		</view>
+		</view>
+		</view>
+	</view>
+	
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'poi',
+				lists: ['A', 'B', 'C', 'D', 'E']
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
